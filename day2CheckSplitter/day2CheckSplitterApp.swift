a73446a938df77618249e9d1fd2c3a34345173a9
Dan <Bdylan998@gmail.com>
2024-01-30 22:08:58 +0200

    Initial Commit

---
+++ day2CheckSplitter/day2CheckSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  day2CheckSplitterApp.swift
+//  day2CheckSplitter
+//
+//  Created by Dainis Putans on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct day2CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
