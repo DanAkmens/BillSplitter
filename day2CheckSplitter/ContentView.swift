83584a8db92581b6ea8bd78bae4dc08bc269b94f
Dan <Bdylan998@gmail.com>
2024-02-01 13:43:51 +0200

    add comments

--- day2CheckSplitter/ContentView.swift
+++ day2CheckSplitter/ContentView.swift
@@ -11,15 +11,30 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-
+    
+    @FocusState private var amountIsFocused: Bool
+    
     let tipPrecentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
 
     var body: some View {
         NavigationStack {
             Form {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    // once the keyboard appears for the check amount entry, it never goes away
+                    // This is a problem with the decimal and number keypads
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -38,9 +53,17 @@ struct ContentView: View {
                 }
                     
                     Section {
-                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     }
                 }.navigationTitle("BillSplit")
+            // lets us specify toolbar items for a view. These toolbar items might appear in various places on the screen – in the navigation bar at the top, in a special toolbar area at the bottom, and so on.
+                .toolbar {
+                    if amountIsFocused {
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
             }
         }
     }
