aae01735781773957f6ee4f90c57410e47f193d7
Dan <Bdylan998@gmail.com>
2024-02-01 12:20:46 +0200

    add new section, segmented picker

--- day2CheckSplitter/ContentView.swift
+++ day2CheckSplitter/ContentView.swift
@@ -20,19 +20,30 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
-                }
-
-                Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2..<100) {
-                        Text("\($0) people")
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                }.pickerStyle(.navigationLink)
+                Section("How much tip do you want to leave?") {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPrecentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
-                .pickerStyle(.navigationLink)
+                    
+                    Section {
+                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }.navigationTitle("BillSplit")
             }
-            .navigationTitle("BillSplit")
         }
     }
-}
 
 #Preview {
     ContentView()
